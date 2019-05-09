49fd0c0ab14984c3e5a52adf46f0b307e7597ba8
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-05-09 17:33:44 +0200

    Initial commit

---
+++ index.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import TestRenderer from "react-test-renderer";
+import Button from "@material-ui/core/Button";
+
+function createNodeMock() {
+  return {
+    nodeType: 1, // to trick findDOMNode
+    // for focus-visible polyfill
+    ownerDocument: {
+      addEventListener: () => {},
+      removeEventListener: () => {}
+    }
+  };
+}
+
+it("can mount a button", () => {
+  const renderer = TestRenderer.create(<Button />, { createNodeMock });
+  expect(renderer.toJSON()).toMatchSnapshot();
+});
